e0b3aa184711c8651f53124a2173d28f55cac3ab
HPPro <alexisgebrah@gmail.com>
2024-12-18 18:10:46 +0200

    rtk-ts

---
+++ src/features/slices/weatherSlice.ts
@@ -0,0 +1,15 @@
+import {createSlice} from "@reduxjs/toolkit";
+import {WeatherInfo} from "../../utils/types";
+
+const initialState: WeatherInfo = {}
+
+const weatherSlice = createSlice({
+    name: 'weather',
+    initialState,
+    reducers: {
+        putWeatherInfo: (_state, action) => action.payload
+    }
+})
+
+export const {putWeatherInfo} = weatherSlice.actions
+export default weatherSlice.reducer
